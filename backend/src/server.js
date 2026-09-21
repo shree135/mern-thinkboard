c171dc080c467ecb78e5109221a3a27ad414e8b0
shree135 <shreyaalicejacob17@gmail.com>
2026-09-21 10:54:37 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,20 @@ import { connectDb } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 
 const app=express();
 const PORT=process.env.PORT || 5001
+const __dirname=path.resolve()
 
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin:"http://localhost:5173"
 }))
+}
 app.use(express.json()) // this middleware will get us accesss to req.body
 app.use(rateLimiter)
 
@@ -26,6 +30,14 @@ app.use(rateLimiter)
 // })
 
 app.use("/api/notes",notesRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 
 //IN rest API , most used are GET ( get some posts ) , POST(Create a post ) , PUT ( Update a post ) , DELETE (Delete a post )
